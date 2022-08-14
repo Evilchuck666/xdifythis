078df95895d64c559bb3012ac4d91a09b605bcf0
EvilChuck <carlos.martinez.medina1992@gmail.com>
2022-08-14 12:11:49 +0200

    First test of the bot

--- main.py
+++ main.py
@@ -13,8 +13,38 @@ auth = tweepy.OAuth1UserHandler(apiKey, apiSecret, accessToken, accessTokenSecre
 
 api = tweepy.API(auth)
 
-user = api.get_user(screen_name='xdifythis')
-#api.update_status(status="Hello World! I'm programming a bot!")
+def getLastTweet(file):
+    f = open(file, 'r')
+    lastId = int(f.read().strip())
+    f.close()
+    return lastId
 
-tweets = api.user_timeline(screen_name='AntonioMiraflo1', count=1)
-print(tweets[0].text)
+def putLastTweet(file, lastId):
+    f = open(file, 'w')
+    f.write(str(lastId))
+    f.close()
+    return
+
+def answerTweets(file='lastId.txt'):
+    lastId = getLastTweet(file)
+
+    mentions = api.mentions_timeline(since_id=lastId, tweet_mode='extended')
+    if len(mentions) == 0:
+        return
+
+    new_id = 1558482514758467586
+
+    for mention in reversed(mentions):
+        newId = mention.id
+        print(mention.full_text)
+
+    putLastTweet(file, newId)
+
+if __name__ == '__main__':
+    answerTweets()
+
+# user = api.get_user(screen_name='xdifythis')
+# #api.update_status(status="Hello World! I'm programming a bot!")
+#
+# tweets = api.user_timeline(screen_name='AntonioMiraflo1', count=1)
+# print(tweets[0].text)
