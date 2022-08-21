4e1b6a91f10e7ffb64e5456da85aa336255e3cb0
EvilChuck <carlos.martinez.medina1992@gmail.com>
2022-08-21 17:21:41 +0200

    Server bash script to run and manage the python bot script.

---
+++ server.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Check if lock file exists
+if [ -f "lock" ]; then
+    echo "";
+    echo "The bot is already running.";
+    echo "If you want to start it again, delete the lock file or wait until the script finishes.";
+    echo "";
+    exit 1;
+fi
+
+# Create lock file
+touch lock;
+
+# Run the bot
+echo "###################################################################";
+echo "########################### RUNNING BOT ###########################";
+echo "###################################################################";
+echo "";
+
+python3 bot.py >> bot.log 2>> error.log;
+
+rm -rf lock;
